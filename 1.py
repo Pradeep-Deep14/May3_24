ee4662750df7114e1d6e20921ffe4f3d9f7150c2
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-05-03 16:50:38 +0530

    first committ

--- 1.py
+++ 1.py
@@ -1,11 +1,13 @@
 class MyClass:
     def __init__(self,x):
-        self.x=x
+        self.x=x  #2
     
     def __call__(self,y):
-        return self.x * y
+        return self.x * y #2*3=6
 
 p1=MyClass(2)
 
 print(p1(3))
         
+
+#6
